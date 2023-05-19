d291008864b8e5cc05fec8e80444b71dbdf6eaf1
Grace Manning <gracebmanning@gmail.com>
2023-05-19 00:12:31 -0700

    remove jquery

---
+++ index.js
@@ -0,0 +1,14 @@
+fetch('./components/navbar.html')
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (body) {
+                document.querySelector('#navbarSection').innerHTML = body;
+            });
+fetch('./components/sidebar.html')
+    .then(function (response) {
+        return response.text();
+    })
+    .then(function (body) {
+        document.querySelector('#sidebarSection').innerHTML = body;
+    });
